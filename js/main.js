3c9f90bd7ea9d3fc4f2b3ff1742b2bc6b8533d2c
EnzoIsrael <fabre@outlook.com>
2014-05-29 15:06:48 -0500

    efecto zoom works

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@ $( document ).ready(function() {
 
 	var theCanvas = document.getElementById('canvas');
 	var context   = theCanvas.getContext('2d');
+	var img = new Image();
+
+	var windowX = 0;
+	var windowY = 0;
+	var currentScale = 1;
+	var minScale = .2;
+	var maxScale = 3;
+	var scaleInc = .1;
+
+	setInterval(eventImgLoaded,100);
 
 	document.getElementById('files')
 	.addEventListener('change', handleFileSelect, false);
 
-	var img = new Image();
 	img.addEventListener('load', eventImgLoaded, false);
 
-	    function handleFileSelect(evt) {
-		    var files = evt.target.files; 
+	    function handleFileSelect(e) {
+		    var files = e.target.files; 
 
 		    for (var i = 0, f; f = files[i]; i++) {
 		      if (!f.type.match('image.*')) {
@@ -30,10 +39,35 @@ $( document ).ready(function() {
 	    }
 
 		function eventImgLoaded(){
-			context.drawImage(img,0,0, img.width, img.height);
+			context.fillStyle = "#cccccc";
+			context.fillRect(0, 0, 500, 500);
+			context.drawImage(img
+				, windowX, windowY
+				, img.width, img.height
+				, 0, 0
+				, img.width*currentScale, img.height*currentScale);
 		}
-		
 
+		document.onkeydown = function(e){
+			e = e?e:window.event;
+
+			switch(e.keyCode){
+
+				case 109:
+					//-
+					currentScale-=scaleInc;
+					if(currentScale<minScale){
+						currentScale = minScale;
+					}
+					break;
+				case 107:
+					//+
+					currentScale+=scaleInc;
+					if(currentScale>maxScale){
+						currentScale = maxScale
+					}
+			}
+		}
 });
 
 
